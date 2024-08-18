c6ff11d3aafa15c87c81fd50f19db6920e06a0ff
CrrtN30 <andi.akbar@student.unismuh.ac.id>
2024-08-18 19:35:34 +0800

    Chapter 1 Done

--- Script/Chapter1.py
+++ Script/Chapter1.py
@@ -1,21 +1,16 @@
-import pygame
+import pygame # type: ignore
 import PIL
 import cv2
-import moviepy
+import moviepy.editor as mp # type: ignore
 import pydub
 import tkinter as tk
-import pkg_resources
 
 def check_installation():
     print("✅ Pygame version:", pygame.__version__)
     print("✅ Pillow version:", PIL.__version__)
     print("✅ OpenCV version:", cv2.__version__)
-    print("✅ MoviePy version:", moviepy.__version__)
-    try:
-        pydub_version = pkg_resources.get_distribution("pydub").version
-        print("✅ Pydub version:", pydub_version)
-    except pkg_resources.DistributionNotFound:
-        print("✅ Pydub version not found")
+    print("✅ MoviePy version:")
+    print("✅ Pydub version:")
     print("✅ Tkinter is installed and working!")
 
 if __name__ == "__main__":
